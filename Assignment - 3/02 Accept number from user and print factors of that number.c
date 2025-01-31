ae997b736a0d99860f062c3a0aeb78ce3ac6e135
MaliSakshi07 <sakshimali467@gmail.com>
2025-01-31 22:22:40 +0530

    Assignment

---
+++ Assignment - 3/02 Accept number from user and print factors of that number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void DisplayFactor(int);
+
+int main()
+{
+    int No = 0;
+
+    printf("Enter a Number :");
+    scanf("%d",&No);
+
+    DisplayFactor(No);
+
+    getch();
+    return 0;
+}
+
+void DisplayFactor(int Num)
+{
+    int i = 0;
+
+    if(Num <= 0)
+    {
+        Num = -Num;
+    }
+
+    for(i=1; i<=Num/2; i++)
+    {
+        if(Num%i == 0)
+        {
+            printf(" %d",i);
+        }
+    }
+}
