d12bb1241396c1b188f83a33e04010e7421fb859
MaliSakshi07 <sakshimali467@gmail.com>
2025-01-31 22:20:31 +0530

    Assignment

---
+++ Assignment - 3/01 Accept one number from user and print that number of even numbers on screen.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void PrintEven(int);
+
+int main()
+{
+    int No = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&No);
+
+    PrintEven(No);
+
+    getch();
+    return 0;
+}
+
+void PrintEven(int Num)
+{
+   int n = 1, c = 1;
+
+   while(c <= Num)
+   {
+      if(n % 2 == 0)
+      {
+          c = c+1;
+          printf("%d\n",n);
+      }
+      n++;
+   }
+
+    return Num;
+}
+
+
