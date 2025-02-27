07dc7bd9dd04f69ccddad3e93d781e5708509ece
MaliSakshi07 <sakshimali467@gmail.com>
2025-02-27 23:22:14 +0530

    Assignment

---
+++ Assignment - 4/01 Write A Program To Accept And Print That Name.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+     char name[40];
+
+     printf("\nEnter Your Name :");
+     scanf("%s",&name);
+
+     printf("\nYour Name is : %s",name);
+
+     getch();
+     return 0;
+ }
