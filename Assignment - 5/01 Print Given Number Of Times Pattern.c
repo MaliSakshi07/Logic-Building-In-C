f976fb213f9d0929935a7d9a6313f163b9aecfc3
MaliSakshi07 <sakshimali467@gmail.com>
2025-02-28 19:25:13 +0530

    Assignment

---
+++ Assignment - 5/01 Print Given Number Of Times Pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+
+void Pattern(Num)
+{
+    int i = 0;
+
+    for(i=1; i<=Num; i++)
+    {
+        printf(" # * ");
+    }
+}
+
+int main()
+{
+    int No = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&No);
+
+    Pattern(No);
+
+    getch();
+}
