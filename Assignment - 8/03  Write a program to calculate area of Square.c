8e97deb2f339ad18e0d933b3b743c7de64d2479f
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-05 23:02:41 +0530

    Assignment

---
+++ Assignment - 8/03  Write a program to calculate area of Square.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int SquareArea(int A)
+{
+    int Area = 0;
+
+    Area = A*A;
+
+    return Area;
+}
+
+int main()
+{
+    int Value = 0, Ret = 0;
+
+    printf("\nEnter a Value :");
+    scanf("%d",&Value);
+
+    Ret = SquareArea(Value);
+
+    printf("\nArea Of Square : %d",Ret);
+
+    getch();
+    return 0;
+}
