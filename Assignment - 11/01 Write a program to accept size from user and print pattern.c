875d5758cbd37dea068f8066bed0022340c498ab
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-09 23:57:27 +0530

    Assignment

---
+++ Assignment - 11/01 Write a program to accept size from user and print pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+void Pattern(int Value)
+{
+  int I = 0;
+  char ch ='A';
+
+  for(I = 1;I <= Value;I++)
+  {
+    printf(" %c # ",ch);
+    ch++;
+  }
+}
+
+int main()
+{
+  int Size = 0;
+
+  printf("\n\t Enter Number of elements => ");
+  scanf("%d",&Size);
+
+  Pattern(Size);
+
+  return 0;
+}
